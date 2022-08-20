91c50876dc992475080f32a8832afff422367d21
Ahmedsha97 <59999635+Ahmedsha97@users.noreply.github.com>
2022-08-20 13:19:46 +0300

    firbase

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
