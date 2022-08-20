ac5cd9fb62f22f8ccc1e88df79f8f852f834ded3
Ahmedsha97 <59999635+Ahmedsha97@users.noreply.github.com>
2022-08-20 14:26:32 +0300

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -17,6 +16,7 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
